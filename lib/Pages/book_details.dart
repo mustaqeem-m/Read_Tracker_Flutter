35cce971f1d6e826eba12527da8d54ce27314042
mustaqeem-m <mmmustaqeem1910@gmail.com>
2025-06-25 17:22:11 +0530

    Added Info in Details Screen

--- lib/Pages/book_details.dart
+++ lib/Pages/book_details.dart
@@ -14,9 +14,43 @@ class _BookDetailsScreenState extends State<BookDetailsScreen> {
   Widget build(BuildContext context) {
     final args = ModalRoute.of(context)?.settings.arguments as BookDetailsArguments; 
     final Book book = args.itemBook;
+    final theme = Theme.of(context);
     return Scaffold(
       appBar: AppBar(
         title: Text(book.title),  
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              if(book.imageLinks.isNotEmpty)
+              Padding(padding: const EdgeInsets.all(18.0),
+              child: Image.network(book.imageLinks['thumbnail'] ?? '',
+              fit: BoxFit.cover,)
+              ),
+              Column(
+                children: [
+                  Text(book.title,
+                  style: theme.textTheme.headlineSmall!.copyWith(fontWeight: FontWeight.bold)),
+                  SizedBox(height: 10,),
+                  Text(book.authors.join(', '),
+                  style: theme.textTheme.labelLarge,),
+                  SizedBox(height: 10,),
+                  Text("Published: ${book.publishedDate}",
+                  style: theme.textTheme.bodyMedium),
+                  SizedBox(height: 10,),
+                  Text("PageCount: ${book.pageCount}",
+                  style: theme.textTheme.bodySmall,),
+                  SizedBox(height: 10,),
+                  Text("Language: ${book.language}",
+                  style: theme.textTheme.bodySmall,),
+
+
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
